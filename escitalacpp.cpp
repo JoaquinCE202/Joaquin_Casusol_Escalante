88dd67fe23eaaf256319e98fdeb1238a49f536c2
JoaquinCE202 <81270957+JoaquinCE202@users.noreply.github.com>
2021-03-26 10:57:36 -0500

    Add files via upload

---
+++ escitalacpp.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+
+
+using namespace std;
+
+
+class Algoritmo {
+  private:
+    int clavef=3,clavec=6;
+  public:
+    int contador=0;
+    int contadory=0;
+    
+  string cifrado (string mensaje)
+  {
+    for (int x;x<clavef*clavec;x++)
+    {
+      if (x>mensaje.length())
+      {
+        mensaje.append(" ");
+      }
+    }
+    string resultado=mensaje;
+    for (int x=0;x<clavec;x++)
+    {
+      for (int y=0;y<clavef;y++)
+      {
+        resultado[y+contadory]=mensaje[x+contador];
+        contador=contador+clavec;
+        }
+      contadory=contadory+clavef;
+      contador=0;
+    }
+    return resultado;
+  }
+
+  string descifrado(string mensaje)
+  {
+    string resultado=mensaje;
+      for (int x=0;x<clavef;x++)
+    {
+      for (int y=0;y<clavec;y++)
+      {
+        resultado[y+contadory]=mensaje[x+contador];
+        contador=contador+clavef;
+        }
+      contador=0;
+      contadory=contadory+clavec;
+    }
+    return resultado;
+
+  }
+};
+
+
+
+int main() {
+  Algoritmo carlos;
+  Algoritmo juan;
+  string mensaje_cifrado=carlos.cifrado("hola como estan");
+  string mensaje_descifrado=juan.descifrado(mensaje_cifrado);
+  cout<<"El mensaje crifrado queda: "<<mensaje_cifrado<<endl;
+  cout<<"El mensaje descifrado queda: "<<mensaje_descifrado<<endl;
+}
+
