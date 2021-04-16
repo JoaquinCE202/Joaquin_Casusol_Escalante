b3522cd19ea36f4db8c7db23ca66668809597da5
JoaquinCE202 <81270957+JoaquinCE202@users.noreply.github.com>
2021-04-16 12:06:07 -0500

    Add files via upload

---
+++ control/pregunta2_control.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+  string arreglo="6939465";
+  int numero;
+  numero=atoi(arreglo.c_str());
+  cout<<numero<<endl;
+  cout<<arreglo<<endl;
+  cout<<arreglo+"veinte"<<endl;
+  cout<<numero*2<<endl;
+}
